1acaf69125f55774de70a0e08f387edec2aa90e4
Aaron Ballard <mraballard@gmail.com>
2016-10-04 19:28:04 -0300

    Edit route successfully loads

--- controllers/users.js
+++ controllers/users.js
@@ -36,19 +36,18 @@ router.get('/reviews', authenticate, function(req, res){
 
 // Users Show Review Route
 router.get('/reviews/:postId', authenticate, function(req, res){
-  console.log('THIS IS THE SHOW POST ROUTE');
   var review = findReview(req.user.reviews, req.params.postId);
-  // .then(function(review){
-    console.log('getting review.....'+ review);
+  console.log(review);
+  var id = review._id.toString();
     res.render('reviews/show', {  //tried passing review: review but it wouldn't load. Had to define each property
-    user: req.user,
+      user: req.user,
       title: review.title,
       location: review.location,
       createdAt: review.createdAt,
       theGood: review.theGood,
-      theBad: review.theBad
+      theBad: review.theBad,
+      id: id
     });
-  // });
 });
 
 // CREATE NEW POST
@@ -74,9 +73,19 @@ router.post('/new', function(req, res){
   res.redirect(`/user/${req.params.userId}`);
 });
 // EDIT POST
-router.get('reviews/:postId/edit', authenticate, function(req, res){
-  console.log(req.user);
-  res.render('reviews/edit', {user: req.user});
+router.get('/reviews/:postId/edit', authenticate, function(req, res){
+  console.log('This is the edit post route.');
+  var review = findReview(req.user.reviews, req.params.postId);
+  var id = review._id.toString();
+  console.log(review);
+  res.render(`reviews/edit`, {
+      user: req.user,
+      title: review.title,
+      location: review.location,
+      theGood: review.theGood,
+      theBad: review.theBad,
+      id: id
+    });
 });
 // PROFILE ROUTE
 router.get('/:userId/profile', authenticate, function(req, res){
